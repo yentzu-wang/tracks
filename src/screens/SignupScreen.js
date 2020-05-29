28deb1cfdbc1c1ded9cc3444c38e514aa6c3764c
Frank <frank7300228@gmail.com>
2020-05-29 17:12:38 +0800

    Finish 15-192

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,10 +1,18 @@
 import React from "react"
-import { StyleSheet, Text, View } from "react-native"
+import { StyleSheet, Text, View, Button } from "react-native"
 
-const SignupScreen = () => {
+const SignupScreen = ({ navigation }) => {
   return (
     <View>
       <Text style={{ fontSize: 48 }}>SignupScreen</Text>
+      <Button
+        title="Go to Signin"
+        onPress={() => navigation.navigate("Signin")}
+      />
+      <Button
+        title="Go to mian flow"
+        onPress={() => navigation.navigate("mainFlow")}
+      />
     </View>
   )
 }
