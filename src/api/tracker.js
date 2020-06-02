25d930625d4eba354ac2b1bce6b98b1667b14a1d
Frank <frank7300228@gmail.com>
2020-06-02 11:27:43 +0800

    Finish 15-201

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 
 export default axios.create({
-  baseURL: "http://4e4a73aeb212.ngrok.io"
+  baseURL: "http://dc5cc529e730.ngrok.io"
 })
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -3,6 +3,8 @@ import trackerApi from "../api/tracker"
 
 const authReducer = (state, action) => {
   switch (action.type) {
+    case "add_error":
+      return { ...state, errorMessage: action.payload }
     default:
       return state
   }
@@ -15,6 +17,10 @@ const signup = dispatch => {
       console.log(response.data)
     } catch (err) {
       console.log(err.message)
+      dispatch({
+        type: "add_error",
+        payload: "Something went wrong with sign up"
+      })
     }
   }
 }
@@ -36,5 +42,5 @@ const signout = dispatch => {
 export const { Provider, Context } = createDataContext(
   authReducer,
   { signin, signout, signup },
-  { isSignedIn: false }
+  { isSignedIn: false, errorMessage: "" }
 )
