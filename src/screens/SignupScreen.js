b703e69856eb77642da01d05bee7d011e5d0556f
Frank <frank7300228@gmail.com>
2020-06-02 17:48:51 +0800

    Finish 15-207

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,41 +1,21 @@
-import React, { useState, useContext } from "react"
+import React, { useContext } from "react"
 import { View, StyleSheet, TouchableOpacity } from "react-native"
-import { Text, Input, Button } from "react-native-elements"
+import { Text } from "react-native-elements"
 import Spacer from "../components/Spacer"
+import AuthForm from "../components/AuthForm"
 import { Context as AuthContext } from "../context/AuthContext"
 
 const SignupScreen = ({ navigation }) => {
   const { state, signup } = useContext(AuthContext)
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
 
   return (
     <View style={styles.container}>
-      <Spacer>
-        <Text h3>Sign Up for Tracker</Text>
-      </Spacer>
-      <Input
-        label="Email"
-        value={email}
-        onChangeText={setEmail}
-        autoCapitalize="none"
-        autoCorrect={false}
+      <AuthForm
+        headerText="Sign Up for Tracker"
+        errorMessage={state.errorMessage}
+        submitButtonText="Sign Up"
+        onSubmit={signup}
       />
-      <Spacer />
-      <Input
-        label="Password"
-        value={password}
-        onChangeText={setPassword}
-        autoCapitalize="none"
-        autoCorrect={false}
-        secureTextEntry
-      />
-      {state.errorMessage ? (
-        <Text style={styles.errorMessage}>{state.errorMessage}</Text>
-      ) : null}
-      <Spacer>
-        <Button title="Sign Up" onPress={() => signup({ email, password })} />
-      </Spacer>
       <TouchableOpacity onPress={() => navigation.navigate("Signin")}>
         <Spacer>
           <Text style={styles.link}>
