339ae62ac24c147abdc2a127afb5e10f0cdecf5a
Frank <frank7300228@gmail.com>
2020-06-04 16:14:10 +0800

    Finish 15-210

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,14 +1,33 @@
 import React from "react"
-import { StyleSheet, Text, View } from "react-native"
+import { StyleSheet, View } from "react-native"
+import AuthForm from "../components/AuthForm"
+import NavLink from "../components/NavLink"
 
 const SigninScreen = () => {
   return (
-    <View>
-      <Text style={{ fontSize: 48 }}>SigninScreen</Text>
+    <View style={styles.container}>
+      <AuthForm
+        headerText="Sign In to Your Account"
+        errorMessage=""
+        onSubmit={() => {}}
+        submitButtonText="Sign In"
+      />
+      <NavLink
+        routeName="Signup"
+        text="Don't have an account? Sign up instead!"
+      />
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+SigninScreen.navigationOptions = { header: null }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    marginBottom: 200
+  }
+})
 
 export default SigninScreen
