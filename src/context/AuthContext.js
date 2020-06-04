ecac9193adaa9cccbf3c30f4e2e11b69f61aed1c
Frank <frank7300228@gmail.com>
2020-06-04 17:06:16 +0800

    Finish 15-213

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -10,9 +10,15 @@ const authReducer = (state, action) => {
     case "signup":
     case "signin":
       return { ...state, token: action.payload, errorMessage: "" }
+    case "clear_error_message":
+      return { ...state, errorMessage: "" }
     default:
       return state
   }
+}
+
+const clearErrorMessage = dispatch => () => {
+  dispatch({ type: "clear_error_message" })
 }
 
 const signup = dispatch => async ({ email, password }) => {
@@ -52,6 +58,6 @@ const signout = dispatch => {
 
 export const { Provider, Context } = createDataContext(
   authReducer,
-  { signin, signout, signup },
+  { signin, signout, signup, clearErrorMessage },
   { token: null, errorMessage: "" }
 )
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,14 +1,16 @@
 import React, { useContext } from "react"
 import { View, StyleSheet } from "react-native"
+import { NavigationEvents } from "react-navigation"
 import AuthForm from "../components/AuthForm"
 import NavLink from "../components/NavLink"
 import { Context as AuthContext } from "../context/AuthContext"
 
 const SignupScreen = () => {
-  const { state, signup } = useContext(AuthContext)
+  const { state, signup, clearErrorMessage } = useContext(AuthContext)
 
   return (
     <View style={styles.container}>
+      <NavigationEvents onWillFocus={clearErrorMessage} />
       <AuthForm
         headerText="Sign Up for Tracker"
         errorMessage={state.errorMessage}
