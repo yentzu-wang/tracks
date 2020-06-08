b724d2bee44bf83733378bdfa16bd5b86677edba
Frank <frank7300228@gmail.com>
2020-06-08 10:21:21 +0800

    Finish 15-218

---
+++ src/components/Map.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { StyleSheet, Text, View } from "react-native"
+
+const Map = () => {
+  return (
+    <View>
+      <Text>Map</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default Map
