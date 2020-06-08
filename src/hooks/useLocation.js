c9b7bfc2d346178973be1726934345bf858a7f4e
Frank <frank7300228@gmail.com>
2020-06-08 16:40:58 +0800

    Finish 15-233

---
+++ src/hooks/useLocation.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react"
+import {
+  Accuracy,
+  requestPermissionsAsync,
+  watchPositionAsync
+} from "expo-location"
+
+export default callback => {
+  const [err, setErr] = useState(null)
+
+  useEffect(() => {
+    startWatching()
+
+    return () => {}
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  async function startWatching() {
+    try {
+      await requestPermissionsAsync()
+      await watchPositionAsync(
+        {
+          accuracy: Accuracy.BestForNavigation,
+          timeInterval: 1000,
+          distanceInterval: 10
+        },
+        location => callback(location)
+      )
+    } catch (e) {
+      setErr(e)
+    }
+  }
+
+  return [err]
+}
