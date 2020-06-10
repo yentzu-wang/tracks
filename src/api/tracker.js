6bb10705def029b3441a957a1f017f4ba14c51e1
Frank <frank7300228@gmail.com>
2020-06-10 16:09:52 +0800

    Finish 15-251

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,21 @@
 import axios from "axios"
+import { AsyncStorage } from "react-native"
 
-export default axios.create({
+const instance = axios.create({
   baseURL: "http://f12ad161c1f3.ngrok.io"
 })
+
+instance.interceptors.request.use(
+  async config => {
+    const token = await AsyncStorage.getItem("token")
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+
+    return config
+  },
+  err => Promise.reject(err)
+)
+
+export default instance
--- src/hooks/useSaveTrack.js
+++ src/hooks/useSaveTrack.js
@@ -1,6 +1,7 @@
 import { useContext } from "react"
 import { Context as TrackContext } from "../context/TrackContext"
 import { Context as LocationContext } from "../context/LocationContext"
+import {Context as AuthContext} from '../context/AuthContext'
 
 export default () => {
   const { createTrack } = useContext(TrackContext)
